b6caa647b2d0c4967f7cbb11e28c8b9d0a637a2c
Anasdarouichi <anassdarouichi@gmail.com>
2023-04-11 16:50:09 -0700

    It's not bragging if you can back it up

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -3,41 +3,41 @@
 
 /**
  * alloc_grid - returns a pointer to a 2 dimensional array of integers.
- * width: is  width of the array.
- * height: is height of the array.
+ * @width: is  width of the array.
+ * @height: is height of the array.
  *
  * Return: pointer of an array of integers
  */
 int **alloc_grid(int width, int height)
 {
-	 int **gridout;
-	 int i, j;
+	int **gridout;
+	int i, j;
 
-	 if (width < 1 || height < 1)
-		 return (NULL);
+	if (width < 1 || height < 1)
+		return (NULL);
 
-	 gridout = malloc(height * sizeof(int *));
-	 if (gridout == NULL)
-	 {
-		 free(gridout);
-		 return (NULL);
-	 }
+	gridout = malloc(height * sizeof(int *));
+	if (gridout == NULL)
+	{
+		free(gridout);
+		return (NULL);
+	}
 
-	 for (i = 0; i < height; i++)
-	 {
-		 gridout[i] = malloc(width * sizeof(int));
-		 if (gridout[i] == NULL)
-		 {
-			 for (i--; i >= 0; i--)
-				 free(gridout[i]);
-			 free(gridout);
-			 return (NULL);
-		 }
-	 } 
+	for (i = 0; i < height; i++)
+	{
+		gridout[i] = malloc(width * sizeof(int));
+		if (gridout[i] == NULL)
+		{
+			for (i--; i >= 0; i--)
+				free(gridout[i]);
+			free(gridout);
+			return (NULL);
+		}
+	}
 
-	 for (i = 0; i < height; i++)
-		 for (j = 0; j < width; j++)
-			 gridout[i][j] = 0;
+	for (i = 0; i < height; i++)
+		for (j = 0; j < width; j++)
+			gridout[i][j] = 0;
 
-	 return (gridout);
+	return (gridout);
 }
--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -3,17 +3,17 @@
 
 /**
  * free_grid - frees a 2 dimensional grid.
- * grid: is multidimensional array of integers.
- * height: is height of the grid.
+ * @grid: is multidimensional array of integers.
+ * @height: is height of the grid.
  *
  * Return: no return
  */
 void free_grid(int **grid, int height)
 {
-	 if (grid != NULL && height != 0)
-	 {
-		 for (; height >= 0; height--)
-			 free(grid[height]);
-		 free(grid);
-	 }
+	if (grid != NULL && height != 0)
+	{
+		for (; height >= 0; height--)
+			free(grid[height]);
+		free(grid);
+	}
 }
