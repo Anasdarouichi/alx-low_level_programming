7ecd5c65725d005335a188f6916e4e8928b0940d
Anasdarouichi <anassdarouichi@gmail.com>
2023-07-06 15:53:23 -0700

    unsigned int

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,37 @@
+/*Write a function that converts a binary number to an unsigned int.
+ *
+ * Prototype: unsigned int binary_to_uint(const char *b);
+ * where b is pointing to a string of 0 and 1 chars
+ * Return: the converted number, or 0 if
+ * there is one or more chars in the string b that is not 0 or 1
+ * b is NULL
+ * author : ANASDAROUICHI
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int ui;
+	int len, base_two;
+
+	if (!b)
+		return (0);
+
+	ui = 0;
+
+	for (len = 0; b[len] != '\0'; len++)
+		;
+
+	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
+	{
+		if (b[len] != '0' && b[len] != '1')
+		{
+			return (0);
+		}
+
+		if (b[len] & 1)
+		{
+			ui += base_two;
+		}
+	}
+
+	return(ui);
+}
